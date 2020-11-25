9363da3e32f48fe1f287220122de3886c3a16a9a
Michael Derzhavets <m.derzhavets@emg-soft.com>
2020-11-25 11:49:02 +0200

    init with button example

---
+++ sock-et/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace socket
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
